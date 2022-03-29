10577ccaaf71f86a7e025b07265f890a45114c43
yushi-okunishi <yushi.okunishi@gmail.com>
2022-03-29 14:56:01 +0900

    fix

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import config as cfg
 def main():
     assert cfg.weight_path is not None, "need to fill in weight_path in config.py"
     assert cfg.result_path is not None, "need to fill in result_path in config.py"
-    det = {}
-    seg = {}
     device = torch.device(cfg.device)
 
     for c in MVTec.CATEGORY:
